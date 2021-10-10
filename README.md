e1adb5d0b1e51d2de2f9621c83450dc17c0dba0e
Ricky <yuen.ricky@hotmail.com>
2021-10-10 14:32:32 -0400

    initialized basic sample hardhat project

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Swap our ETH for WETH
+2. Deposit ETH into AAVE
+3. Borrow asset with ETH collateral
+4. Short shell asset
+5. Repay collateral
---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function getWeth() {
+  /**
+   * Deposit ETH to mint WETH
+   */
+}
+
+async function main() {}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
